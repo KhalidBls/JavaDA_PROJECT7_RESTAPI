4622f2f284e398a50ea5fb1b14328c5c4a4c592a
Khalid Boulssane <khalid.boulssane@hotmail.com>
2020-10-20 11:46:00 +0200

    mise en place des logs pour un user qui se connecte

--- Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
+++ Poseiden-skeleton/src/main/java/com/nnk/springboot/controllers/BidListController.java
@@ -2,13 +2,20 @@ package com.nnk.springboot.controllers;
 
 import com.nnk.springboot.domain.BidList;
 import com.nnk.springboot.repositories.BidListRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 @Controller
@@ -17,9 +24,15 @@ public class BidListController {
     @Autowired
     BidListRepository bidListRepository;
 
+    private final Logger logger = LoggerFactory.getLogger(BidListController.class);
+
 @RequestMapping("/bidList/list")
     public String home(Model model)
     {
+        DateFormat format = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Calendar calendar = Calendar.getInstance();
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        logger.info(((UserDetails)principal).getUsername() + " is connected at "+ format.format(calendar.getTime()) +", Authority : "+ ((UserDetails)principal).getAuthorities().toString());
         model.addAttribute("bidLists", bidListRepository.findAll());
         return "bidList/list";
     }
